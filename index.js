eda4e3d67f87bd42dec66155545bfc02d5ce715a
DESKTOP-MBRQ737\Eva <evastylova@gmail.com>
2023-03-22 21:17:37 +0100

    Du Formular Lekce9

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log('funguju!');
+
+const user = {
+	email: 'bartolomej.rozumbrada@gmail.com',
+	password: 'vimzenicnevim',
+	name: 'Bartoloměj',
+}
+
+document.querySelector('#login').addEventListener('submit', (event) => {
+    event.preventDefault ()
+
+    const emailInput = document.querySelector('#email-input')
+    const passwordInput = document.querySelector('#password-input')
+    const container = document.querySelector('.container')
+    const status = document.querySelector('#status')
+
+    if (
+        emailInput.value === user.email && passwordInput.value === user.password
+
+    ) {
+        container.innerHTML = `<h1>Přihlášený uživatel: ${user.name}</h1>`
+    } else {
+        status.textContent = 'Neplatné přihlašovací údaje'
+        passwordInput.value = ''
+    }
+})
